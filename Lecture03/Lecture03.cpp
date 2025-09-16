667ee98225898cdd2adb80eb44c8d35f929dad23
vulcanus3521 <jinwook232@naver.com>
2025-09-16 18:29:59 +0900

    gg

---
+++ Lecture03/Lecture03.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+void sum(int x, int y, int *re)
+{
+    *re = x + y;
+}
+
+int main()
+{
+    int a = 3;
+    int b = 2;
+    int c = 0;
+
+    /*int* pa;
+    pa = &a;
+    printf("%d", *pa);
+    */
+
+    sum(a, b, &c);
+
+    printf("%d", c);
+
+    return 0;
+}
