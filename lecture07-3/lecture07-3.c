59af173d6a5db8d7d4da2fb2804463750c1ee3bb
4nor1ker <4nor1ker@gmai.com>
2025-10-14 17:26:12 +0900

    10.14

---
+++ lecture07-3/lecture07-3.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int arr[4] = { 4,3,2,1 };
+	int* pArr;
+
+	pArr = (int*)malloc(sizeof(int) * 4);
+
+	if (pArr == NULL)
+	{
+		printf("malloc error");
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		pArr[i] = arr[i];
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		printf("%d \n", pArr[i]);
+	}
+
+	free(pArr);
+
+	system("pause");
+	return 0;
+}
